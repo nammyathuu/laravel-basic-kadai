025a01e4bf72e92c669f6ee8f99a4aa729ab2811
nammyatyuu <namichan2533@yahoo.co.jp>
2024-08-13 22:54:23 +0900

    変更

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,9 @@ Route::get('/', function () {
 
 Route::get('/posts', [PostController::class, 'index']);
 
+Route::post('/posts/store', [PostController::class, 'store']);
+
 Route::get('/posts/{id}', [PostController::class, 'show']);
 
 Route::get('/posts/create', [PostController::class, 'create']);
 
-Route::post('/posts/store', [PostController::class, 'store']);
