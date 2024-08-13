8f40e8f44b94777993e5d29c8372ee819abbf766
nammyatyuu <namichan2533@yahoo.co.jp>
2024-08-14 00:14:39 +0900

    変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -33,6 +33,6 @@ class PostController extends Controller
         $post->content = $request->input('content') ;
         $post->save() ;
 
-        return redirect("/posts/{$post->id}") ;
+        return redirect("/posts") ;
      } 
 }
