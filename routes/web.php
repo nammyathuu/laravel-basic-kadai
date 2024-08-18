458a6bae0c12a241f49fb8d43603453a275c4c98
nammyatyuu <namichan2533@yahoo.co.jp>
2024-08-18 20:58:40 +0900

    変更

--- routes/web.php
+++ routes/web.php
@@ -38,15 +38,15 @@ Route::get('/post', function () {
     return 'Hello, world!';
 });
 
-//投稿一覧
+
 Route::get('/posts',[PostController::class, 'index'])->name('posts.index');
 
-//投稿作成
+
 Route::get('/posts/create',[PostController::class, 'create'])->name('posts.create')->middleware('auth');
 
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 
- //投稿詳細
+ 
  Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
  Route::get('/posts/{id}', [PostController::class, 'show']);
